615ae04e966c85566db518190c12a20fb3568440
Alp <alpasyon07@gmail.com>
2025-03-26 22:39:29 +0000

    string commit keys

--- src/hooks/supabaseHook.ts
+++ src/hooks/supabaseHook.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	"https://ypvkgzjlhoerzavdxotx.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwdmtnempsaG9lcnphdmR4b3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTUyMDEsImV4cCI6MjA1ODU5MTIwMX0.c7RkfL43aP0NZcsFc-GvsXHcVpIBB1pPO8v_0Yd6PbU"
 );
 
 export const useCreateClient = () => {
